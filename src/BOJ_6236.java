bce72bcf72ebb9ca258cf4332da58636f6aab851
kangaroo <qlsdud960604@gmail.com>
2021-05-01 16:01:52 +0900

    백준 6236번 문제 풀이

---
+++ src/BOJ_6236.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_6236 {
+	static int N;
+	static int M;
+
+	static int[] arr;
+
+	static int max = 0;
+	static int total = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+
+			max = Math.max(max, arr[i]);
+			total += arr[i];
+		}
+
+		bw.write(binarySearch() + "\n");
+		bw.close();
+	}
+
+	static int binarySearch() {
+		int start = max;
+		int end = total;
+
+		while (start <= end) {
+			int middle = (start + end) / 2;
+
+			int count = 1;
+			int sum = 0;
+
+			for (int i = 0; i < N; i++) {
+				if (sum + arr[i] > middle) {
+					count++;
+					sum = 0;
+				}
+
+				sum += arr[i];
+			}
+
+			if (count <= M)
+				end = middle - 1;
+			else
+				start = middle + 1;
+		}
+		return start;
+	}
+}
