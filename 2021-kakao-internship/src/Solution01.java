d34d6231f21972ba83dfe81a1190097ad53732bc
임빈영 <qlsdud960604@gmail.com>
2021-08-19 21:11:52 +0900

    2021 카카오 인턴십 코딩 테스트 1번 문제 풀이

---
+++ 2021-kakao-internship/src/Solution01.java
@@ -0,0 +1,15 @@
+
+public class Solution01 {
+	public int solution(String s) {
+		int answer = 0;
+
+		String[] number = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		String[] word = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+		for (int i = 0; i < 10; i++) {
+			s = s.replace(word[i], number[i]);
+		}
+
+		return Integer.parseInt(s);
+	}
+}
