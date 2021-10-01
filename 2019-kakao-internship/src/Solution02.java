4c964a1899fdb39f5cb5b4f6447d7528ee6e5f74
임빈영 <qlsdud960604@gmail.com>
2021-10-01 18:53:56 +0900

    2019 카카오 인턴십 코딩 테스트 2번 문제 풀이

---
+++ 2019-kakao-internship/src/Solution02.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Solution02 {
+	static int[] answer;
+	static int[] number = new int[100000 + 1];
+
+	static ArrayList<Element> list = new ArrayList<>();
+
+	public static void main(String[] args) {
+
+		String s = "{{20,111},{111}}";
+		solution(s);
+
+		for (int i = 0; i < answer.length; i++)
+			System.out.print(answer[i] + " ");
+	}
+
+	static public int[] solution(String s) {
+		String[] input = s.split("\\{|\\}|\\,");
+
+		/** 주어진 문자열 탐색 */
+		for (int i = 0; i < input.length; i++) {
+			if (!input[i].equals("")) {
+				int index = Integer.parseInt(input[i]);
+				number[index]++;
+			}
+		}
+
+		/** 원소 탐색 */
+		for (int i = 0; i < number.length; i++) {
+			if (number[i] != 0) {
+				list.add(new Element(i, number[i]));
+			}
+		}
+
+		Collections.sort(list);
+
+		answer = new int[list.size()];
+
+		for (int i = 0; i < list.size(); i++)
+			answer[i] = list.get(i).number;
+
+		return answer;
+	}
+
+	/** 원소에 대한 정보를 정의 */
+	static class Element implements Comparable<Element> {
+		int number; // 원소 번호
+		int count; // 원소의 빈도수
+
+		Element(int number, int count) {
+			this.number = number;
+			this.count = count;
+		}
+
+		@Override
+		public int compareTo(Element o) {
+			return o.count - this.count;
+		}
+	}
+}
