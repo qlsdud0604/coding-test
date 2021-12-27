219ed1c66c4a45ebec7b47800e3e91711ff442cb
임빈영 <qlsdud960604@gmail.com>
2021-12-27 11:39:59 +0900

    모의고사 문제 풀이

---
+++ programmers-level01/src/Solution08.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution08 {
+
+	public static void main(String[] args) {
+		int[] answers = { 1, 2, 3, 4, 5 };
+		solution(answers);
+	}
+
+	static public int[] solution(int[] answers) {
+		int[] person01 = { 1, 2, 3, 4, 5 };
+		int[] person02 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+		int[] person03 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+		int[] point = new int[3];
+
+		for (int i = 0; i < answers.length; i++) {
+			if (person01[i % person01.length] == answers[i])
+				point[0]++;
+			if (person02[i % person02.length] == answers[i])
+				point[1]++;
+			if (person03[i % person03.length] == answers[i])
+				point[2]++;
+		}
+
+		int max = Math.max(Math.max(point[0], point[1]), point[2]);
+
+		List<Integer> list = new ArrayList<>();
+
+		if (max == point[0])
+			list.add(1);
+		if (max == point[1])
+			list.add(2);
+		if (max == point[2])
+			list.add(3);
+
+		int[] answer = new int[list.size()];
+
+		for (int i = 0; i < list.size(); i++)
+			answer[i] = list.get(i);
+
+		return answer;
+	}
+}
